bc2a8089219d98bf19d8e922c2ba4a8dec5ffe8e
kshetritej <tejghartikshetri@gmail.com>
2026-03-14 20:51:20 +0545

    change: services images

--- components/sections/services-section.tsx
+++ components/sections/services-section.tsx
@@ -10,9 +10,9 @@ export default function ServiceSection() {
         </p>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
           <div className="relative lg:row-span-2">
-            <div className="absolute inset-px rounded-lg bg-gray-800 lg:rounded-l-4xl" />
+            <div className="absolute inset-px rounded-lg bg-background lg:rounded-l-4xl" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+              <div className="px-8 pb-3 sm:px-10 sm:pb-0">
                 <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
                   Full Stack Engineering
                 </p>
@@ -23,10 +23,10 @@ export default function ServiceSection() {
                 </p>
               </div>
               <div className="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 outline outline-white/20">
+                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-3xl border-t-[1cqw] border-black bg-gray-900 outline outline-white/20">
                   <img
                     alt=""
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                    src="/digital-code.png"
                     className="size-full object-cover object-top"
                   />
                 </div>
@@ -35,7 +35,7 @@ export default function ServiceSection() {
             <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-white/15 lg:rounded-l-4xl" />
           </div>
           <div className="relative max-lg:row-start-1">
-            <div className="absolute inset-px rounded-lg bg-gray-800 max-lg:rounded-t-4xl" />
+            <div className="absolute inset-px rounded-lg bg-background max-lg:rounded-t-4xl" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
               <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                 <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
@@ -58,7 +58,7 @@ export default function ServiceSection() {
             <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-white/15 max-lg:rounded-t-4xl" />
           </div>
           <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-            <div className="absolute inset-px rounded-lg bg-gray-800" />
+            <div className="absolute inset-px rounded-lg bg-background" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
               <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                 <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
@@ -81,7 +81,7 @@ export default function ServiceSection() {
             <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-white/15" />
           </div>
           <div className="relative lg:row-span-2">
-            <div className="bg-indigo-80 absolute inset-px rounded-lg bg-gray-800 max-lg:rounded-b-4xl lg:rounded-r-4xl" />
+            <div className="bg-indigo-80 absolute inset-px rounded-lg bg-background max-lg:rounded-b-4xl lg:rounded-r-4xl" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
               <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                 <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
@@ -94,19 +94,14 @@ export default function ServiceSection() {
                 </p>
               </div>
               <div className="relative min-h-120 w-full grow">
-                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900/60 outline outline-white/10">
-                  <div className="flex bg-gray-900 outline outline-white/5">
-                    <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                      <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                        NotificationSetting.jsx
-                      </div>
-                      <div className="border-r border-gray-600/10 px-4 py-2">
-                        App.jsx
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 pt-6 pb-14">
-                    {/* Your code example */}
+                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-background outline outline-white/10">
+                  <div className="max-lg: @container flex flex-1 items-center lg:pb-2">
+                    <img
+                      alt=""
+                      src="/strategic-ui-ux.png"
+                      className="object-cover"
+                      // className="h-[min(152px,40cqw)] object-cover"
+                    />
                   </div>
                 </div>
               </div>
