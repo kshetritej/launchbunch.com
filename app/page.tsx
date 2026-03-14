1c1e692b8ed950feca269e2f340c4df7da83115f
kshetritej <tejghartikshetri@gmail.com>
2026-03-14 17:11:22 +0545

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
+import Navbar from "@/components/navbar"
 import ContactSection from "@/components/sections/contact-section"
+import Cta from "@/components/sections/cta"
 import FAQSection from "@/components/sections/faq-section"
 import FeatureSection from "@/components/sections/feature-section"
 import Hero from "@/components/sections/hero-section"
-import LogoCloud from "@/components/sections/logo-cloud"
+import ServiceSection from "@/components/sections/services-section"
+import Stats from "@/components/sections/stats"
 import SmoothScrolling from "@/components/smooth-scrolling"
 
 export default function Page() {
   return (
     <SmoothScrolling>
+      <Navbar />
       <Hero />
+      <Stats />
       <FeatureSection />
+      <ServiceSection />
       <ContactSection />
       <FAQSection />
+      <Cta />
     </SmoothScrolling>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,37 @@
+import { Button } from "@base-ui/react"
+import Logo from "./logo"
+
 export default function Navbar() {
-  return <div>Navbar</div>
+  const navigationData = [
+    {
+      title: "Home",
+      href: "#",
+    },
+    {
+      title: "Products",
+      href: "#",
+    },
+    {
+      title: "About Us",
+      href: "#",
+    },
+    {
+      title: "Contacts",
+      href: "#",
+    },
+  ]
+
+  return (
+    <div className="container mx-auto flex justify-between p-4">
+      <Logo />
+      <div className="flex gap-4">
+        {navigationData.map((item, index) => (
+          <a key={index} href={item.href}>
+            {item.title}
+          </a>
+        ))}
+      </div>
+      <Button>Contact Now</Button>
+    </div>
+  )
 }
